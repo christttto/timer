8120cee31260f9e2e369b6622bb8670f11ec1a13
Kris <christttto@berkeley.edu>
2019-02-22 16:19:24 -0800

    set up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React, { Component } from "react"
 class App extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      timeLeft: {}
+    }
   }
+
   render() {
     return <div>sanity check</div>
   }
