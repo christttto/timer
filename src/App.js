e9d6d74612e4ad9ae6cfd213b884f83ed9eea25b
Kris <christttto@berkeley.edu>
2019-02-24 17:30:21 -0800

    basic function works yay

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ class App extends Component {
       minuteLeft: 0,
       secondLeft: 0,
       totalSecond: 0,
+      sandPosition: 0,
+      sandStep: 0,
       isCounting: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.countdown = this.countdown.bind(this)
+    this.sandFlow = this.sandFlow.bind(this)
   }
   handleChange(event) {
     const { name, value } = event.target
@@ -24,21 +27,39 @@ class App extends Component {
   handleSubmit(event) {
     event.preventDefault()
     const mySecond = this.state.minuteLeft * 60 + this.state.secondLeft
-    this.setState({ totalSecond: mySecond })
+    const mySandStep = 600 / mySecond
+    this.setState({ totalSecond: mySecond, sandStep: mySandStep })
     this.countdown()
   }
-
+  sandFlow(move) {
+    return (
+      <img
+        style={{
+          //moving this...maybe make a method that renders the image given speicifc position
+          position: "absolute",
+          top: move /**take a variable in */,
+          left: "0px",
+          height: "300px"
+        }}
+        src={Sand}
+        alt="Sand"
+      />
+    )
+  }
   countdown() {
     //when clicked several times, expediates the process
     const myDown = setInterval(() => {
       if (this.state.totalSecond < 2) {
         clearInterval(myDown)
       }
+
       this.setState(prevState => {
         return {
-          totalSecond: prevState.totalSecond - 1
+          totalSecond: prevState.totalSecond - 1,
+          sandPosition: Math.floor(prevState.sandPosition + prevState.sandStep)
         }
       })
+      console.log(this.state.sandPosition)
     }, 1000)
   }
   //how to create sand falling off
@@ -47,20 +68,11 @@ class App extends Component {
     return (
       <div>
         <div>
-          <img
-            style={{
-              position: "absolute",
-              top: "0px",
-              left: "0px",
-              height: "600px"
-            }}
-            src={Sand}
-            alt="Sand"
-          />
+          {this.sandFlow(this.state.sandPosition)}
           <img
             style={{
               position: "relative",
-              height: "1200px"
+              height: "600px"
             }}
             src={Hourglass}
             alt="Hourglass"
@@ -81,7 +93,6 @@ class App extends Component {
           />
           <button>start</button>
         </form>
-        {Hourglass.clientWidth}
       </div>
     )
   }
